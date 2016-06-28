7f8cc8979953c983a315a42cd2997fb061b83cd8
feyrkh <kevinhobbs@gmail.com>
2016-06-27 21:35:31 -0500

    Added stacks of ingredients

---
+++ Assets/ColorMesh.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorMesh : MonoBehaviour {
+    public Color color;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Renderer>().material.color = color;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
