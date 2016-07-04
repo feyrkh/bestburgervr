bea6788abe56789aefde897b203b1ef4d99c4410
kevinhobbs@gmail.com <kevinhobbs@gmail.com>
2016-07-04 15:02:24 -0500

    Clock prefab and script.

---
+++ Assets/ClockTimer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClockTimer : MonoBehaviour {
+
+	public float secondsToRun = 30;
+	private float secondsElapsed = 0;
+	public Transform hand;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (secondsElapsed < secondsToRun) {
+			secondsElapsed += Time.deltaTime;
+			if (secondsElapsed > secondsToRun)
+				secondsElapsed = secondsToRun;
+			if (secondsElapsed >= secondsToRun) {
+				SendMessage ("OnClockTimerElapsed", this);
+			}
+			hand.localRotation = Quaternion.Euler(new Vector3 (0, 0, 360 * (secondsElapsed / secondsToRun)));
+		}
+	}
+
+	public void ResetClock() {
+		secondsElapsed = 0;
+	}
+}
