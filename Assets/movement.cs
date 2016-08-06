306e56c3a2599d2fcbf50e33693696bffd141dc7
jeremy <jascony@gmail.com>
2016-08-06 14:55:19 -0500

    set up camera movement in game again. messed around with walls in the main scene.

---
+++ Assets/movement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+	float x, y, z;
+	public float xSpeed, ySpeed, zSpeed;
+	void Update () {
+		x = Input.GetAxis ("Horizontal") * Time.deltaTime * xSpeed;
+		z = Input.GetAxis ("Horizontal") * Time.deltaTime * zSpeed;
+		y = Input.GetAxis ("Horizontal") * Time.deltaTime * ySpeed;
+
+
+	}
+}
